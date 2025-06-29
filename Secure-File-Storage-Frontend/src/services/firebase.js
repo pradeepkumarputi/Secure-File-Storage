d3adea4de2a5f07bd7af34f246ecea1d13bda313
pradeepkumarputi <pradeepkumarputi8@gmail.com>
2025-06-29 14:05:48 +0530

    api

--- Secure-File-Storage-Frontend/src/services/firebase.js
+++ Secure-File-Storage-Frontend/src/services/firebase.js
@@ -10,6 +10,8 @@ import {
   onAuthStateChanged
 } from "firebase/auth";
 
+console.log("FIREBASE CONFIG:", firebaseConfig);
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
